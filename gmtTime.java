99ea650116067e400e0af34fb965f7818380a80d
Jaghanraj Srinivasan <Jaghanraj.Srinivasan@ATMECS.CORP>
2022-05-11 19:02:56 +0530

    Test Folder

---
+++ gmtTime.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Time {
+
+    public static void main(String[] Strings) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input the time zone offset to GMT: ");
+        long timeZoneChange = input.nextInt();
+
+        long totalMilliseconds = System.currentTimeMillis();
